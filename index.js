0d41a40772c46955a5bef519f79f61583171445d
Aida <edigeeva.aika@mail.ru>
2023-02-23 08:35:43 +0600

    sorted html, js, css

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 //1. У нас есть массив ["Banana", "Cola", "Cat"] вывести их на страницу в виде карточек.
+
+const items = ["Banana", "Cola", "Cat"];
+const container = document.getElementById("card-container");
+
+for (let i = 0; i < items.length; i++) {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.innerText = items[i];
+  container.appendChild(card);
+}
+
 //2. У нас есть массив ["hand", "leg", "head", "finger", "eye"] вывести элементы массива в виде списка.
