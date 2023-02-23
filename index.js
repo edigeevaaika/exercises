a0d4fecaee4801b001138a5d12369a21e80a05c8
Aida <edigeeva.aika@mail.ru>
2023-02-23 10:05:31 +0600

    solution of the fourth task

--- index.js
+++ index.js
@@ -35,4 +35,29 @@ for (let i = 0; i < greetings.length; i++) {
 document.body.appendChild(container);
 // 4.BoomBam! У нас есть массив [["1", "2", "3", "4", "5", "6"], ["7", "8", "9", "10", "11", "12"], ["13", "14", "15", "16", "17", "18"], ["19", "20", "21", "22", "23", "24"], ["25", "26", "27", "28", "29", "30"]] вывести этот массив в виде таблицы.
 
+const data = [
+  ["1", "2", "3", "4", "5", "6"],
+  ["7", "8", "9", "10", "11", "12"],
+  ["13", "14", "15", "16", "17", "18"],
+  ["19", "20", "21", "22", "23", "24"],
+  ["25", "26", "27", "28", "29", "30"],
+];
+const table = document.createElement("table");
 
+for (let i = 0; i < data.length; i++) {
+  const row = document.createElement("tr");
+
+  for (let j = 0; j < data[i].length; j++) {
+    const cell = document.createElement("td");
+    cell.innerText = data[i][j];
+    row.appendChild(cell);
+  }
+
+  table.appendChild(row);
+}
+
+document.body.appendChild(table);
+table.style.fontSize = "50px";
+table.style.paddingTop = "70px";
+table.style.paddingLeft = "70px";
+table.style.color = "blue";
